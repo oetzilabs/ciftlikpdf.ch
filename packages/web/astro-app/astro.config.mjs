f8c1600dbeaebc9efb820c72447703930d281f35
Özgür Isbert <oezguerisbert@gmail.com>
2024-02-06 16:48:37 +0100

    chores

--- packages/web/astro-app/astro.config.mjs
+++ packages/web/astro-app/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import aws from "astro-sst";
 import sitemap from "@astrojs/sitemap";
+import aws from "astro-sst";
 
 export default defineConfig({
-  adapters: aws(),
-  output: "server",
   integrations: [tailwind(), solidJs(), sitemap()],
+  output: "server",
+  adapters: aws(),
 });
