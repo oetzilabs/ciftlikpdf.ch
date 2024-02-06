a47b6cb3f1852e853e9f7df64e464880ffc7f8b7
Özgür Isbert <oezguerisbert@gmail.com>
2024-02-06 16:14:24 +0100

    chores: output mode

--- packages/web/astro-app/astro.config.mjs
+++ packages/web/astro-app/astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   adapters: aws(),
-  output: "server",
   integrations: [tailwind(), solidJs(), sitemap()],
   site: "https://ciftlikpdf.ch",
 });
