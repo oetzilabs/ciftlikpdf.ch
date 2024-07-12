f3b3d4386bec87d30e9726807e706c0e0b5d14a2
Özgür Isbert <oezguerisbert@gmail.com>
2024-07-12 16:48:45 +0200

    chores: cleanup

--- packages/functions/src/migrator.ts
+++ packages/functions/src/migrator.ts
@@ -1,12 +1,15 @@
 import { migrate } from "@ciftlikpdf/core/drizzle/sql";
 import { ApiHandler } from "sst/node/api";
 import { Config } from "sst/node/config";
+import { error, json } from "./utils";
 
 export const handler = ApiHandler(async (_evt) => {
   console.log(`Migrating to ${Config.DATABASE_URL} ...`);
-  await migrate();
+  try {
+    await migrate();
+  } catch (e) {
+    return error((e as Error).message);
+  }
 
-  return {
-    body: "Migrated!",
-  };
+  return json({ message: "Migration successful" });
 });
--- packages/functions/src/utils.ts
+++ packages/functions/src/utils.ts
@@ -1,20 +1,4 @@
-import { verifyJwt } from "@ciftlikpdf/core/auth";
-import { User } from "@ciftlikpdf/core/entities/users";
 import { StatusCodes } from "http-status-codes";
-import { useHeader } from "sst/node/api";
-
-export const getUser = async () => {
-  const header = useHeader("authorization");
-  if (!header) throw new Error("No authorization header");
-  const [type, token] = header.split(" ");
-  if (type !== "Bearer") throw new Error("Invalid authorization header");
-  console.log("token", token);
-  const userid = await verifyJwt(token);
-  if (!userid) throw new Error("Invalid JWT");
-  const user = await User.findById(userid);
-  if (!user) throw new Error("No session found");
-  return user;
-};
 
 export const json = (input: unknown, statusCode = StatusCodes.OK) => {
   return {
