eff16940a115d4f6b39a41a8db945060d6ab09c8
Özgür Isbert <oezguerisbert@gmail.com>
2026-01-02 22:00:24 +0100

    cleanup+updates

--- packages/core/src/drizzle/sql/index.ts
+++ packages/core/src/drizzle/sql/index.ts
@@ -1,8 +1,5 @@
 import { createClient } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql";
-import { migrate as mig } from "drizzle-orm/libsql/migrator";
-import { join } from "path";
-import { Resource } from "sst";
 import * as schema from "./schema";
 
 const client = createClient({ url: process.env.DATABASE_URL!, authToken: process.env.DATABASE_TOKEN! });
--- stacks/solid-start.ts
+++ stacks/solid-start.ts
@@ -2,10 +2,12 @@ import { domain } from "./domain";
 import { SECRET } from "./secrets";
 
 new sst.cloudflare.x.SolidStart("MySolidStart", {
-  path: "packages/v2",
-  link: [SECRET],
+  path: "packages/app",
   domain,
   environment: {
     SST_STAGE: $app.stage,
+    DATABASE_URL: SECRET.DATABASE_URL.value,
+    DATABASE_TOKEN: SECRET.DATABASE_AUTH_TOKEN.value,
+    JWT_SECRET: SECRET.JWT_SECRET.value,
   },
 });
