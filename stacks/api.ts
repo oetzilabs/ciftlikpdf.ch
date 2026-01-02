b40a1bd44241082e21a11b7d2c77d688eaa86dbc
Özgür Isbert <oezguerisbert@gmail.com>
2026-01-02 16:55:53 +0100

    wip: moving from aws to cf

--- stacks/api.ts
+++ stacks/api.ts
@@ -1,4 +1,9 @@
 const worker = new sst.cloudflare.Worker("PdfWorker", {
-  path: "packages/functions",
+  hander: "packages/functions/src/pdf-generate.main",
+  url: true,
+});
+
+const migrator = new sst.cloudflare.Worker("MigratorWorker", {
+  hander: "packages/functions/src/migrator.handler",
   url: true,
 });
